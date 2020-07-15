174dd3e53fab8f758180615a5af10c909e5977be
Garrett <goodbyetonto@gmail.com>
2020-07-14 21:48:06 -0700

    working wines model and 'find-all' get request

--- routes/wine-search-api-routes.js
+++ routes/wine-search-api-routes.js
@@ -2,24 +2,29 @@ const db = require("../models");
 
 module.exports = (app) => {
     // Find all wines that match filtered search and return them to the user with res.json
-    app.get("/api/wines/:variety?/:price?/:vintage?", (req, res) => {
+    app.get("/api/wines/:variety", (req, res) => {
         db.wines.findAll({
             where: {
-                variety: req.params.variety,
-                price: req.params.price,
-                vintage: req.params.vintage
+                variety: req.params.Variety,
+                // price: req.params.Price,
+                // vintage: req.params.Vintage
             }
         })
         .then((dbWines) =>  {
             res.json(dbWines); 
-        }); 
+        })
+        .catch((err) => {
+            if(err) return console.log(err); 
+        })
     }); 
 
     app.get("/api/wines/", (req, res) => {
         db.wines.findAll({})
         .then((dbWines) =>  {
             res.json(dbWines); 
-        }); 
+        }).catch((err) => {
+            if(err) return console.log(err); 
+        })
     }); 
 
 }
