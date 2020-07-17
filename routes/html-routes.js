dc5c486d85dac5fab3c5c919ecdf461c572a9d9f
Garrett <goodbyetonto@gmail.com>
2020-07-16 19:18:05 -0700

    All html routes are working now

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,32 +4,35 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the cellars page
+    // If the user already has an account send client 'members.html'
     if (req.user) {
-      res.redirect("/cellars");
+      res.redirect("/members");
     }
+    // Otherwise send client 'signup.html'
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
-  // Send client the 'Login' html page
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
+    // If the user already authenticated and hits '/login' again, send client 'members.html'
     if (req.user) {
-      res.redirect("/cellars");
+      res.redirect("/members");
     }
+    // Otherwise, send client 'login.html'
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/cellars", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/cellars.html"));
+  app.get("/members", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+  // Send client 'search.html'
+  app.get("/search", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  })
 };
 
-//sends client the 'Wine Search' html page
-app.get("/search", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/search.html")); 
-}); 
+
