c8c3ce2e365fffdadfb30626717916b7153e216b
Garrett <goodbyetonto@gmail.com>
2020-07-21 12:04:29 -0700

    Added some basic form validation on search.js for the 'Vintage' filter search. Using regex, validating that input is both not a blank string, nor a string containing alpha characters. Cleaned up the default form value for the 'Vintage' input field in search.html

--- public/js/search.js
+++ public/js/search.js
@@ -30,7 +30,7 @@ $(document).ready(() => {
         $(varietyBtn).on("click", () => {
             const varietyVal = varietyInput.val();
             cards.empty();
-            $.post("/api/wines/variety", {variety: varietyVal} ).then((data) => {
+            $.post("/api/wines/variety", { variety: varietyVal }).then((data) => {
                 console.log(data);
                 for (let i = 0; i < data.length; i++) {
                     let varietyCard =
@@ -50,28 +50,33 @@ $(document).ready(() => {
 
 
         $(vintageBtn).on("click", () => {
-            const vintageVal = vintageInput.val(); 
-            cards.empty();
-            $.post("/api/wines/vintage", {vintage: vintageVal} ).then((data) => {
-                if (data.length === 0) {
-                    window.alert("There are no wines with this vintage. Please enter another vintage.")
-                } else {
-                    console.log(data.length); 
-                    for (let i = 0; i < data.length; i++) {
-                        let vinCard =
-                            `<div class="card" style= "width: 33%">
-                <div class= "card-body>
-                <h5 class= "card-title"> ${data[i].Title} </h5>
-                <p class= "card-text"> Price: ${data[i].Price}, Vintage: ${data[i].Vintage}, 
-                Points: ${data[i].Points}, Winery: ${data[i].Winery}, 
-                Designation: ${data[i].Designation}, Variety: ${data[i].Designation}
-                </p>
-                </div>
-                </div>`;
-                        cards.append(vinCard);
+            const vintageVal = vintageInput.val();
+            const numbers = /^[0-9]\d*$/;
+            if (vintageVal === "" || !(vintageVal.match(numbers))) {
+                window.alert("You did not enter a proper vintage. Enter the last two digits of the vintage year you would like to search for.")
+            } else {
+                cards.empty();
+                $.post("/api/wines/vintage", { vintage: vintageVal }).then((data) => {
+                    if (data.length === 0) {
+                        window.alert("There are no wines with this vintage. Please enter another vintage.")
+                    } else {
+                        console.log(data.length);
+                        for (let i = 0; i < data.length; i++) {
+                            let vinCard =
+                                `<div class="card" style= "width: 33%">
+                                <div class= "card-body>
+                                <h5 class= "card-title"> ${data[i].Title} </h5>
+                                <p class= "card-text"> Price: ${data[i].Price}, Vintage: ${data[i].Vintage}, 
+                                Points: ${data[i].Points}, Winery: ${data[i].Winery}, 
+                                Designation: ${data[i].Designation}, Variety: ${data[i].Designation}
+                                </p>
+                                </div>
+                                </div>`;
+                            cards.append(vinCard);
+                        }
                     }
-                }
-            });
+                });
+            }
         });
 
         $(priceBtn).on("click", () => {
